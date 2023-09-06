76569bea32ee956af2d3e6d475ed10618e55d5d9
MaksimMyakishevv <130791600+MaksimMyakishevv@users.noreply.github.com>
2023-09-06 22:45:33 +0300

    2dz

--- 2dz.py
+++ 2dz.py
@@ -1,2 +1,18 @@
+n = int(input())
+mas =[]
+mas_bad=[]
 a = 0
-b = 0
+for i in range(n):
+    x = int(input())
+    mas.append(x)
+for i in range(n-1):
+    if mas[a+1]-mas[a] != 1:
+        mas_bad.append(a+1)
+    a+=1
+if not mas_bad:
+    print("Ничего не найдено")
+else:
+    print(mas_bad)
+
+
+
